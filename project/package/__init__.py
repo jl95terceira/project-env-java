0030e20e88a0199dae21dc49e9e14dce9d1f1fb4
Silva Joao <Joao.Silva@tridonic.com>
2024-10-28 15:56:48 +0000

    (no message)

---
+++ project/package/__init__.py
@@ -0,0 +1,16 @@
+import os.path
+
+from jl95terceira.pytools.envlib import var
+
+JDK_HOMES        = var(name       ='jdk.homes', 
+                       description='a map (dict) of Java homes by version (as a string)')
+MAVEN_HOME       = var(name       ='maven.home', 
+                       description='the home of Apache Maven')
+MAVEN            = var(name       ='maven', 
+                       description='the path / alias to Apache Maven executable',
+                       default    = os.path.join(MAVEN_HOME.get(), 'bin', 'mvn.exe') if MAVEN_HOME.check() else 'mvn')
+ANT_HOME         = var(name       ='ant.home', 
+                       description='the home of Apache Ant')
+ANT              = var(name       ='ant', 
+                       description='the path / alias to Apache Ant executable',
+                       default    = os.path.join(ANT_HOME.get(), 'bin', 'mvn.exe') if ANT_HOME.check() else 'ant')
